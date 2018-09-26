7ad48241aeaed9b26186cda26244b8cc7e75072b
Ursha Munikar <ursha.munikar12@gmail.com>
2018-09-26 09:35:39 +0545

    implemented and tested as function

---
+++ application/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 15;
+           double sum= Add(a, b);
+           Console.WriteLine("sum={0}", sum);
+        }
+        static double Add(double a,double b)
+        {
+            return a + b;
+        }
+    }
+}
